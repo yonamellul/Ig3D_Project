33d95113f84c406eef7e7e91153aaf4b2509b9b6
yonamellul <73967090+yonamellul@users.noreply.github.com>
2023-06-20 23:05:44 +0200

    Add files via upload

---
+++ scene_renderer.py
@@ -0,0 +1,39 @@
+import pygame as pg
+import moderngl as mgl
+class SceneRenderer:
+    def __init__(self, app):
+        self.app = app
+        self.ctx = app.ctx
+        self.mesh = app.mesh
+        self.scene = app.scene
+        # depth buffer
+        self.depth_texture = self.mesh.texture.textures['depth_texture']
+        self.depth_fbo = self.ctx.framebuffer(depth_attachment = self.depth_texture )
+
+    def render_shadow(self):
+        self.depth_fbo.clear()
+        self.depth_fbo.use()
+        for obj in self.scene.objects:
+            obj.render_shadow()
+        #print(self.depth_fbo.read(attachment=-1))
+        
+
+    def main_render(self):
+        self.app.ctx.screen.use()
+        for obj in self.scene.objects:
+            obj.render()
+        self.scene.skybox.render()
+    
+    def render(self):
+        self.scene.update()
+        # pass 1
+        self.ctx.enable(mgl.CULL_FACE)
+        self.ctx.front_face = 'ccw'
+        self.ctx.cull_face = 'front'
+        self.render_shadow()
+        # pass 2
+        self.ctx.disable(mgl.CULL_FACE)
+        self.main_render()
+    
+    def destroy(self):
+        self.depth_fbo.release()
